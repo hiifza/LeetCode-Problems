304bb65d5ee5a45ed56900e70faf2a8750bc0498
Hifza <hifzaa1007@gmail.com>
2026-02-24 10:25:09 +0530

    Time: 10 ms (68.47%) | Memory: 44.2 MB (97.75%) - LeetSync

---
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int maxHeight(int[][] cuboids) {
+        for (int[] cuboid : cuboids) {
+            Arrays.sort(cuboid);
+        }
+
+        Arrays.sort(cuboids, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[1] != b[1]) return a[1] - b[1];
+            return a[2] - b[2];
+        });
+
+        int n = cuboids.length;
+        int[] dp = new int[n];
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = cuboids[i][2];
+            for (int j = 0; j < i; j++) {
+                if (cuboids[j][0] <= cuboids[i][0] &&
+                    cuboids[j][1] <= cuboids[i][1] &&
+                    cuboids[j][2] <= cuboids[i][2]) {
+                    dp[i] = Math.max(dp[i], dp[j] + cuboids[i][2]);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        return max;
+    }
+}
